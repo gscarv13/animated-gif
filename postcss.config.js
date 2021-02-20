2e28e0387bf3c0069fc249f352c68b2f772ccf77
Gustavo Carvalho <gsc13@tuta.io>
2021-02-20 14:44:23 -0300

    Install autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
+    require('tailwindcss'),//('./app/javascript/stylesheets/tailwind.config.js'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
@@ -11,3 +12,14 @@ module.exports = {
     })
   ]
 }
+
+// if (process.env.RAILS_ENV === "production") {
+//   environment.plugins.push(
+//     require('@fullhuman/postcss-purgecss')({
+//       content: ['.app/**/*.html.erb', '.app/**/*.rb'],
+//       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//     })
+//   )
+// }
+
+// module.exports = environment
