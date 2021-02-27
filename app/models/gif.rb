0427cb52bcddf16885b340c1beb3884cd6fbd249
Gustavo Carvalho <gsc13@tuta.io>
2021-02-27 17:50:52 -0300

    Add uploaded by to user show, Add sorted method

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -4,6 +4,8 @@ class Gif < ApplicationRecord
 
   acts_as_taggable
 
+  scope :sorted, -> { order(created_at: :desc) } # sort by the most recent when using the sorted method
+
   def self.random
     order('RANDOM()').first
   end
